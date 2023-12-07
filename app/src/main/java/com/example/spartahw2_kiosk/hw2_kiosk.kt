2ffefd50ccb002893abb6463e553ea189cbf3cbe
SoungHyun Choi <sounghyun35@naver.com>
2023-12-07 18:25:02 +0900

    버거주문 구현

--- app/src/main/java/com/example/spartahw2_kiosk/hw2_kiosk.kt
+++ app/src/main/java/com/example/spartahw2_kiosk/hw2_kiosk.kt
@@ -3,13 +3,11 @@ package com.example.spartahw2_kiosk
 fun main (){
     var price=arrayOf<Double>()
     val display= Display()
-    var result=0.0
 
-    var i=0
     while (true) {
         display.mainMenu()      //메인메뉴
 
-        var num1 = readLine()!!.toInt()
+        val num1 = readLine()!!.toInt()
         when (num1) {
             1 -> {
                 display.burgerMenu()        //상세메뉴
@@ -22,7 +20,7 @@ fun main (){
             }
             //2,3,4 추가
             0 -> {
-                println("주문을 종료합니다.")
+                println("주문을 종료합니다. ")
                 break
             }
             else -> println("올바른 메뉴를 선택해주세요")
@@ -33,12 +31,12 @@ fun main (){
 
 fun burgerMenu():Array<Double>{
 
-    var num2 = readln().toIntOrNull()
+    val num2 = readln().toIntOrNull()
     when (num2){
         0 -> return emptyArray()
         1 -> {
             val choice=MenuChoice("ShackBurger",6.9)
-            var pricearr=choice.choice()
+            val pricearr=choice.choice()
             return pricearr
         }
         //2,3,4,5 추가
